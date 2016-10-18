abcda53c7b494720baaed16e75ccca87960b5ea6
Vladislav Trofimov <stranger.65536@gmail.com>
2016-10-19 00:25:47 +0300

    Entire commit

---
+++ vpworkspace/File System Manager.cpp
@@ -0,0 +1,11 @@
+#include "File System Manager.h"
+
+Class diagram::File_System_Manager::File_System_Manager() {
+	// TODO - implement {class}::{operation}
+	throw "Not yet implemented";
+}
+
+Class diagram::File_System_Manager::File_System_Manager() {
+	// TODO - implement {class}::{operation}
+	throw "Not yet implemented";
+}
